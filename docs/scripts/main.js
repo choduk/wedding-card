bccff04fe253e6893bb08fc7c0048f194d54a234
Duk <choduk88@gmail.com>
2022-09-27 04:52:36 +0900

    Reset invitation

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -11,34 +11,20 @@ $(document).ready(function () {
         return false;
     });
 
-    $(".gift-send").click(function () {
-        $("#gift-name").text($(this).data("name"));
-    })
+    function rand(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
+    // 갤러리가 몇개 없으니 대충 복붙
+    var portraitImageNames = ["portrait1.jpeg", "portrait2.jpeg", "portrait3.jpeg", "portrait4.jpeg", "portrait5.jpeg"]
+    var landscapeImageNames = ["landscape1.jpeg", "landscape2.jpeg", "landscape3.jpeg", "landscape4.jpeg"]
 
-    $("#reserveGiftButton").click(function () {
-        let name = $("#sender-name").val();
-        let message = $("#sender-message").val();
-        $("#reserveGiftButton").text("전송중...");
-        $("#reserveGiftButton").prop("disabled", true);
-
-        emailjs.init("user_yjLL5xG0A3kkOCH5BGIDh");
-        emailjs.send("wedding-mail", "gift_send", {
-            name: name,
-            gift: $("#gift-name").text(),
-            message: message
-        }).then(function (response) {
-            $('#giftMailModal').modal('hide');
-            alert(name + "님의 메시지가 정상적으로 전송되었습니다.");
-
-            $("#reserveGiftButton").text("예약하기!");
-            $("#sender-name").val('');
-            $("#sender-message").val('');
-            $("#reserveGiftButton").prop("disabled", false);
-        }, function (err) {
-            alert("메시지 전송이 실패했습니다. 다시 시도해주세요.");
-        });
-    })
+    $(".card a img")[0].src = "images/" + portraitImageNames.splice(rand(0, portraitImageNames.length), 1)
+    $(".card a img")[1].src = "images/" + landscapeImageNames.splice(rand(0, landscapeImageNames.length), 1)
+    $(".card a img")[2].src = "images/" + landscapeImageNames.splice(rand(0, landscapeImageNames.length), 1)
+    $(".card a img")[3].src = "images/" + portraitImageNames.splice(rand(0, portraitImageNames.length), 1)
+    $(".card a img")[4].src = "images/" + portraitImageNames.splice(rand(0, portraitImageNames.length), 1)
+    $(".card a img")[5].src = "images/" + landscapeImageNames.splice(rand(0, landscapeImageNames.length), 1)
 })
 
 // Smooth scroll for links with hashes
