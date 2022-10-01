e642c5956bbc5955b6200cfa01c26c9f71b25cdc
Duk <choduk88@gmail.com>
2022-10-01 15:48:04 +0900

    Add dropdown accounts (with sample)

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -10,11 +10,6 @@ $(document).ready(function () {
         $('html,body').animate({scrollTop: 0}, 400);
         return false;
     });
-
-    function rand(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-
     // 갤러리가 몇개 없으니 대충 복붙
     var portraitImageNames = ["portrait1.jpeg", "portrait2.jpeg", "portrait3.jpeg", "portrait4.jpeg", "portrait5.jpeg"]
     var landscapeImageNames = ["landscape1.jpeg", "landscape2.jpeg", "landscape3.jpeg", "landscape4.jpeg"]
@@ -25,7 +20,45 @@ $(document).ready(function () {
     $(".card a img")[3].src = "images/" + portraitImageNames.splice(rand(0, portraitImageNames.length), 1)
     $(".card a img")[4].src = "images/" + portraitImageNames.splice(rand(0, portraitImageNames.length), 1)
     $(".card a img")[5].src = "images/" + landscapeImageNames.splice(rand(0, landscapeImageNames.length), 1)
+
+    setCoupleAccounts()
 })
+
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function setCoupleAccounts() {
+    addGroomAccounts(couple.groom.accounts)
+    addBrideAccounts(couple.bride.accounts)
+}
+
+function addGroomAccounts(accounts) {
+    accounts.forEach(function(account) {
+        $("#groom-accounts").append(createAccountItemElement(account));
+    })
+}
+
+function addBrideAccounts(accounts) {
+    accounts.forEach(function(account) {
+        $("#bride-accounts").append(createAccountItemElement(account));
+    })
+}
+
+function createAccountItemElement(accountInfo) {
+    return `<div class="dropdown-content-container">
+               <div class="bank-account-item">카카오뱅크 1234-56-78910<br>김카뱅</div>
+               <div class="quick-link-item">
+                   <a href="http://kko.to/cqsSb1RLo"><img class="map-icon" src="images/icon/pay-qr.png" style="width: 30px"/></a>
+                   <a href="http://kko.to/cqsSb1RLo"><img class="map-icon" src="images/icon/pay-logo.png" style="width: 30px"/></a>
+               </div>
+           </div>`
+}
+
+function getContact(coupleType, prefix) {
+    console.log(prefix + ":" + couple[coupleType]["phone_number"])
+    document.location.href = prefix + ":" + couple[coupleType]["phone_number"]
+}
 
 // Smooth scroll for links with hashes
 $("a.smooth-scroll").click(function (event) {
